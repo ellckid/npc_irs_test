4815bd35ad78b54086fc8d5a1dbc5859feecbe24
Артем Лобов <artemlobov@192.168.1.157>
2023-06-27 20:53:04 +0300

    changed sql_init

--- db_init.sql
+++ db_init.sql
@@ -1,6 +1,4 @@
-// не совсем понимаю зачем нужен sql для создания таблиц если мы создаем модели, которые автоматически инициализируют нужные таблицы.
-
-create TABLE shop(
+create TABLE shops(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   date DATE,
@@ -8,23 +6,24 @@ create TABLE shop(
   workers INTEGER
 );
 
-create TABLE product(
+create TABLE products(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
   date DATE,
   price NUMERIC,
   amount INTEGER,
-  shop_id INTEGER,
-  FOREIGN KEY (shop_id) REFERENCES shop (id)
+  shopId INTEGER,
+  FOREIGN KEY (shopId) REFERENCES shop (id)
 );
 
-//заполнять можно либо отправляя запросы через Postman
+INSERT INTO shops (name, date, balance, workers) values ('first shop', '01.01.2023', '1000.00', '1');
+INSERT INTO shops (name, date, balance, workers) values ('second shop', '01.02.2023', '2000.00', '2');
+INSERT INTO shops (name, date, balance, workers) values ('third shop', '01.03.2023', '3000.00', '3');
+INSERT INTO shops (name, date, balance, workers) values ('fourth shop', '01.04.2023', '4000.00', '4');
+INSERT INTO shops (name, date, balance, workers) values ('fifth shop', '01.05.2023', '5000.00', '5');
 
-// POST запрос localhost/api/shop 
-// Тело запроса
-{
-    "name": "first shop",
-    "date": "01.01.2023",
-    "balance": 1000.0,
-    "workers": 1
-}
+INSERT INTO products (name, date, price, amount, shopId ) values ('first product', '01.01.2023', '1000.00', '1', '1');
+INSERT INTO products (name, date, price, amount, shopId ) values ('second product', '01.02.2023', '2000.00', '2', '2');
+INSERT INTO products (name, date, price, amount, shopId ) values ('third product', '01.03.2023', '3000.00', '3', '1');
+INSERT INTO products (name, date, price, amount, shopId ) values ('fourth product', '01.04.2023', '4000.00', '4', '2');
+INSERT INTO products (name, date, price, amount, shopId ) values ('fifth product', '01.05.2023', '5000.00', '5', '3');
